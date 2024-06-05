948c3800a75f974ff51471ced3090d982e722378
nguyentientho.bkhn <nguyentientho.bkhn@gmail.com>
2024-06-05 10:41:42 +0700

    three commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import path from 'path';
 import express from 'express';
 import morgan from 'morgan';
 import { engine } from 'express-handlebars';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger  
 app.use(morgan('combined'));
